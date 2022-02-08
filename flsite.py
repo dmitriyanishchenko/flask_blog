4b76ec73084f43bd46c6b7132dc1af40aed961cc
Dmitry <1348010@gmail.com>
2022-02-08 22:11:50 +0300

    Динамический URL-адрес
    Signed-off-by: Dmitry <1348010@gmail.com>

--- flsite.py
+++ flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -8,13 +8,23 @@ menu = ['Установка', 'Первое приложение', 'Обратн
 @app.route('/index')
 @app.route('/')
 def index():
+    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 
 @app.route('/about')
 def about():
+    print(url_for('about'))
     return render_template('about.html', title="О сайте", menu=menu)
 
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь: {username}"
+
+
+# with app.test_request_context():
+#     print( url_for('about'))
 
 if __name__ == '__main__':
     app.run(debug=True)
