b27740cdc265ddd4f79bb32c2b346ef3469820a2
Dmitry <1348010@gmail.com>
2022-02-13 18:10:35 +0300

    added decorator 'errorhandler', function 'redirect' and 'abort'
    Signed-off-by: Dmitry <1348010@gmail.com>

--- flsite.py
+++ flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, redirect, session, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'GGDFGJIGJ347r9c74cry9r4y93c'
@@ -30,6 +30,29 @@ def contact():
             flash('Ошибка отправки', category='error')
 
     return render_template('contact.html', title="Обратная связь", menu=menu)
+
+
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template('page404.html', title="Cтраница не найдена", menu=menu)
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+    return f"Профиль пользователя:  {username}"
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'dmitry' and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
 
 
 if __name__ == '__main__':
