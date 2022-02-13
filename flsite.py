d3f76090445b39914a43b7f7f62624bd916c6f06
Dmitry <1348010@gmail.com>
2022-02-13 17:00:39 +0300

    added 'get flash messages'
    Signed-off-by: Dmitry <1348010@gmail.com>

--- flsite.py
+++ flsite.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'GGDFGJIGJ347r9c74cry9r4y93c'
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
         {"name": "Обратная связь", "url": "contact"},
@@ -24,7 +24,10 @@ def about():
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == "POST":
-        print(request.form['username'])
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
 
     return render_template('contact.html', title="Обратная связь", menu=menu)
 
