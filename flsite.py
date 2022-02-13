206149613bf4284916721c04e7450ed079d4c382
Dmitry <1348010@gmail.com>
2022-02-13 14:27:01 +0300

    added form 'contact'
    Signed-off-by: Dmitry <1348010@gmail.com>

--- flsite.py
+++ flsite.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = ['Установка', 'Первое приложение', 'Обратная связь']
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"},
+        ]
 
 
 @app.route('/index')
@@ -18,9 +21,12 @@ def about():
     return render_template('about.html', title="О сайте", menu=menu)
 
 
-@app.route("/profile/<username>")
-def profile(username):
-    return f"Пользователь: {username}"
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        print(request.form['username'])
+
+    return render_template('contact.html', title="Обратная связь", menu=menu)
 
 
 if __name__ == '__main__':
